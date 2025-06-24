c54aa05c7124c531d5eb2eec24593d14099d22cb
Hoahong123456789 <120949961+Hoahong123456789@users.noreply.github.com>
2025-06-24 14:45:36 +0700

    Create Banana.sh

---
+++ Banana.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+	rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+. <(curl https://raw.githubusercontent.com/u400822/setup-termux/refs/heads/main/termux-change-repo.sh)
+yes | pkg upgrade && yes | pkg i python && yes | pkg i android-tools && yes | pkg i python-pip && pip install requests psutil prettytable
+curl -Ls "https://raw.githubusercontent.com/u400822/setup-termux/refs/heads/main/Rejoin.py" -o /sdcard/Download/Rejoin.py
